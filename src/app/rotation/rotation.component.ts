879df47e62433cf932d449b3d2e09860f31d1761
El-Yobin <El-Yobin@users.noreply.github.com>
2020-12-16 20:18:16 +0300

    hey

--- src/app/rotation/rotation.component.ts
+++ src/app/rotation/rotation.component.ts
@@ -1,31 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { P5JSInvoker } from '../p5JSInvoker';
 import * as p5Methods from 'p5';
 
 @Component({
   selector: 'app-rotation',
   templateUrl: './rotation.component.html',
-  styleUrls: ['./rotation.component.scss']
+  styleUrls: ['../agario/agar.component.scss']
 })
-export class RotationComponent extends P5JSInvoker implements OnInit {
+export class RotationComponent extends P5JSInvoker implements AfterViewInit {
   public p5: any;
 
   constructor() {
     super();
+  }
+
+  public ngAfterViewInit(): void {
     this.startP5JS(document.getElementById('container'));
   }
 
-  ngOnInit(): void {
+  public preload(p5): void {
   }
 
-  preload(p5): void {
+  public setup(p5): void {
     this.p5 = p5.createCanvas(1000, 600);
+    p5.rectMode(p5.CENTER);
   }
 
-  setup(p5): void {
-  }
-
-  draw(p5): void {
+  public draw(p5): void {
+    this.p5.background(255);
+    this.p5.translate(this.p5.width / 2, this.p5.height / 2);
+    p5.fill(100);
+    p5.rotate(p5.QUARTER_PI);
+    p5.rect(0, 0, 200, 40);
   }
 
 }
